869721229d5e47ab84a3224db6f7981d70746dcc
Roger Yao <lichieh.yao@icloud.com>
2023-02-26 23:15:17 +0800

    Add taint to control node and Add a hint to the script indicating that it must be executed with a version argument.

--- longhorn-tool/longhorn-install.sh
+++ longhorn-tool/longhorn-install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Please provide the version argument."
+    echo "Usage: ./longhorn-install.sh <version>"
+    exit 1
+fi
+
 VERSION=$1
+
+control_nodes=$(kubectl get node | awk '$3 ~ /control-plane/ {print $1}')
+
+for node in ${control_nodes}; do
+  kubectl taint node ${node} node-role.kubernetes.io/master=true:NoExecute
+  kubectl taint node ${node} node-role.kubernetes.io/master=true:NoSchedule
+done
+
+echo "Tainted control-plane nodes:"
+kubectl get node --show-labels | grep node-role.kubernetes.io/master=true
 
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/${VERSION}/deploy/prerequisite/longhorn-nfs-installation.yaml
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/${VERSION}/deploy/prerequisite/longhorn-iscsi-installation.yaml
